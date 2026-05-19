6fed762c0b79cee173b0060e382241aba8572cc6
NamishRajyaguru <namishrajyaguru@gmail.com>
2026-05-19 11:28:36 +0530

    Add Test5.java and .gitignore for oracleJdk-26

---
+++ Test5.java
@@ -0,0 +1,14 @@
+// perfect square
+public class Test5{
+    public static void main(String[] args){
+        int i = 5;
+        while (true){
+            int square = i * i;
+            i++;
+            if(square > 80){
+                break;
+            }
+            System.out.println(square);
+        }    
+    }
+}
