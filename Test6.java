e0239f480584c1bf92b3b8a32269f4f72c6b8451
NamishRajyaguru <namishrajyaguru@gmail.com>
2026-05-19 13:58:14 +0530

    19-5-2026

---
+++ Test6.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Test6{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+        int i = start;
+        while (true) { 
+            if(i * i <= end && i * i >= start){
+                System.out.println(i * i);
+            }
+            if (i * i > end){
+                break;
+            }
+        }
+    }
+}
---
+++ Test7.java
@@ -0,0 +1,18 @@
+// Fibonacci series
+import java.util.*;
+
+public class Test7{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = 0;
+        int b = 1;
+        int sum = 0;
+        for(int i = 1; i <= n; i++){
+            System.out.println(sum);
+            sum = a + b;
+            a = b;
+            b = sum;
+        }
+    }
+}
