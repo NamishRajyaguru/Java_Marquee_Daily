19e1a61ffb8351db2505679564f8671738060085
NamishRajyaguru <namishrajyaguru@gmail.com>
2026-05-20 08:01:14 +0530

    Euclidean Algo of GCD

---
+++ Test8.java
@@ -0,0 +1,14 @@
+//Euclidean Algorithm of GCD
+
+public class Test8{
+    public static void main(String[] args){
+        int max = 60;
+        int min = 18;
+        while(min != 0){
+            int rem = max % min;
+            max = min;
+            min = rem;
+        }
+        System.out.println(max);
+    }
+}
