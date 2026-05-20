b97ee80f4c271978a25bd3465433cbc26d2404c9
NamishRajyaguru <namishrajyaguru@gmail.com>
2026-05-20 09:05:13 +0530

    Matrix Pattern and Right angle traingle

---
+++ Test11.java
@@ -0,0 +1,14 @@
+// Right angle Triangle Pattern 1
+public class Test11 {
+    public static void main(String[] args) {
+
+        // Write your code here
+        int s = 5;
+        for(int i = 0; i <= s; i++){
+            for(int j = 0; j < i; j++){
+                System.out.print("*" + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
