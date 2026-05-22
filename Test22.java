b8628e0e2ef1fac0d0077a375894d72bba302dde
NamishRajyaguru <namishrajyaguru@gmail.com>
2026-05-22 11:06:05 +0530

    More patterns (hollow and staircase)

---
+++ Test22.java
@@ -0,0 +1,24 @@
+// Hollow Pyramid
+
+public class Test22{
+    public static void main(String[] args){
+        int n = 5;
+        for(int i = 0; i < n; i++){
+            for(int j = n; j >= i; j--){
+                System.out.print(" ");
+            }
+            for(int k = 0; k <= i; k++){
+                if (i == 0 || i == n - 1){
+                    System.out.print("* ");
+                }
+                else if ((k == 0 || k == i)){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
