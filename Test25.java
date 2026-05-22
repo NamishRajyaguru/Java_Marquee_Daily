feaf2746c7252aec86aad6356f39c3a1473230b9
NamishRajyaguru <namishrajyaguru@gmail.com>
2026-05-22 12:00:05 +0530

    block staircase pattern

---
+++ Test25.java
@@ -0,0 +1,15 @@
+// Block staircase pattern
+
+public class Test25{
+    public static void main(String[] args){
+        int n = 5;
+        for(int i = 0; i < n ; i++){
+            for(int k = 0; k <= 1; k++){
+                for(int j = 0; j < 2 * i; j++){
+                    System.out.print("* ");
+                }
+            System.out.println();
+            }
+        }
+    }
+}
