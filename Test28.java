6aef079f5e052bab2aa21de26e29f9db3f751039
NamishRajyaguru <namishrajyaguru@gmail.com>
2026-05-23 09:22:55 +0530

    function to check if character is vowel

---
+++ Test28.java
@@ -0,0 +1,22 @@
+// Function to check if character is vowel or not
+
+import java.util.Scanner;
+
+public class Test28{
+    
+    static boolean vowel(char c){
+        char[] array = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        for(char vow : array){
+            if (vow == c){
+                return true;
+            }
+        } 
+        return false;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+        System.out.println(vowel(c));
+    }
+}
