19812b26dd55f56abbc557ad67022a1c06c82095
NamishRajyaguru <namishrajyaguru@gmail.com>
2026-05-25 09:34:25 +0530

    some more functions questions 25/05/2026

---
+++ Functions/Test06.java
@@ -0,0 +1,20 @@
+// Early Function Termination
+
+import java.util.Scanner;
+
+public class Test06{
+
+    public static void add(int a, int b){
+        if (a == 2){
+            return;
+        }
+        System.out.println(a + b);
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        add(a, b);
+    }
+}
---
+++ Functions/Test11.java
@@ -0,0 +1,27 @@
+// Decimal to Binary
+
+import java.util.Scanner;
+
+public class Test11 {
+
+    public static String decimalToBinary(int n) {
+        if (n == 0) return "0";
+        String res = "";
+        while (n > 0) {
+            int rem = n % 2;
+            n /= 2;
+            res = res.concat(Integer.toString(rem));
+        }
+        String rev = "";
+        for (int i = res.length() - 1; i >= 0; i--) {
+            rev += res.charAt(i);
+        }
+        return rev;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(decimalToBinary(n));
+    }
+}
