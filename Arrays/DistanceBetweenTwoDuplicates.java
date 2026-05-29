80c22137914dfd7e8337080aa0b2faf8cb1dde32
NamishRajyaguru <namishrajyaguru@gmail.com>
2026-05-29 09:02:50 +0530

    array - max distance between two duplicates

---
+++ Arrays/DistanceBetweenTwoDuplicates.java
@@ -0,0 +1,38 @@
+// Max distance between two duplicate elements
+
+import java.util.Scanner;
+
+public class DistanceBetweenTwoDuplicates{
+
+    public static int max(int a, int b){
+        if(a > b){
+            return a;
+        }
+        return b;
+    }
+
+    public static int findDistance(int[] arr, int n){
+        int res = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                if(i != j && arr[i] == arr[j]){
+                    if(res == -1){
+                        res = j - i;
+                    }
+                    res = max(res, j - i);
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(findDistance(arr, n));
+    }
+}
