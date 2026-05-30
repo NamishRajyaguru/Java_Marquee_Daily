0d93b8bcd95a49df080734a567014df8cf290832
NamishRajyaguru <namishrajyaguru@gmail.com>
2026-05-30 11:05:12 +0530

    subtraction of arrays

---
+++ Arrays/SubtractionOfIndividualElementsOfTwoArray.java
@@ -0,0 +1,30 @@
+// Subtraction of individual elements of two arrays
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SubtractionOfIndividualElementsOfTwoArray{
+
+    public static int[] subtract(int[] a, int[] b, int n){
+        int[] res = new int[n];
+        for(int i = 0; i < n; i++){
+            res[i] = a[i] - b[i];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++){
+            a[i] = sc.nextInt();
+        }
+        int[] b = new int[n];
+        for(int i = 0; i < n; i++){
+            b[i] = sc.nextInt();
+        }
+
+        System.out.println(Arrays.toString(subtract(a, b, n)));
+    }
+}
